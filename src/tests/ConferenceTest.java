6238463aa45418963f3cf2431640dc7b71ac2f2e
ijj <ijj@mac2.lan>
2017-04-28 12:47:17 -0700

    Added clone to subProgram, filled out methods to reviewerTest

---
+++ src/tests/ConferenceTest.java
@@ -0,0 +1,79 @@
+package tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class ConferenceTest {
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@Test
+	public void testConference() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSubmitPaper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemovePaper() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetPapers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetRoles() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddReviewer() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetReviewers() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAddSubprogramChair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRemoveSubprogramChair() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsPastSubmissionDeadline() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsAuthorPaperLimit() {
+		fail("Not yet implemented");
+	}
+
+}
