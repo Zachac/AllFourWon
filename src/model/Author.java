61d6133b8b0d34c1f236c3144d67fc7e6a251fd0
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-04-30 17:19:55 -0700

    removed login methods from author

--- src/model/Author.java
+++ src/model/Author.java
@@ -8,12 +8,9 @@ package model;
  */
 public class Author extends AbstractRole {
 
-	/**
-	 * 
-	 */
+
+    /** Generated UID.*/
 	private static final long serialVersionUID = 4787057477135476176L;
-	private String loginInfo;
-	private String myName;
 	
 	/**
 	 * Constructs an Author object given a name.
@@ -21,35 +18,5 @@ public class Author extends AbstractRole {
 	 */
 	public Author(String name) {
 	    super(name);
-	    myName = name;
-	    loginInfo = null;
     }
-	
-	/**
-	 * Placeholder login information setup. 
-	 * @param login String used to login
-	 */
-	public void setLogin(String login) {
-		loginInfo = login;
-	}
-	
-	/**
-	 * Simple placeholder login verification method.
-	 * @param login login information
-	 * @return if login was done successfully
-	 */
-	public boolean login(String login) {
-		if(login.equals(loginInfo)) 
-			return true;
-		return false;
-	}
-	
-	/**
-	 * Gets the name of the author.
-	 * @return authors name
-	 */
-	public String getName() {
-		return myName;
-	}
-
 }
