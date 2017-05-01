cadd1f2d4cd0699d15a8d35c3f6f75b6e4e77c16
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-04-30 20:17:36 -0700

    Implemented ConferenceManager

--- src/model/ConferenceManager.java
+++ src/model/ConferenceManager.java
@@ -1,6 +1,8 @@
 package model;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class ConferenceManager implements Serializable {
@@ -9,23 +11,52 @@ public class ConferenceManager implements Serializable {
 	 * Generated serial version UID
 	 */
 	private static final long serialVersionUID = -6646261571061047225L;
-
+	
+	private List<Conference> conferences;
+	
+	
+	public ConferenceManager () {
+	    conferences = new LinkedList<Conference>();
+	}
+	
 	/**
 	 * Adds a conference to a list.
-	 * @param c conference object
-	 * @return if the conference was successfully added
+	 * @param c conference object.
+	 * @throws NullPointerException if the given conference is null.
+	 * 
+	 * if the conference is already being managed,
+	 * the result will be true, and the function will not effect anything.
+	 * 
+	 * @return if the conference was successfully added.
 	 */
-	boolean addConference(Conference c) {
-		return false;
+	boolean addConference(Conference c) throws NullPointerException {
+	    if (c == null) {
+	        throw new NullPointerException("Cannot add null conferences!");
+	    }
+	    
+	    if (conferences.contains(c)) {
+	        return true;
+	    }
+	    
+		return conferences.add(c);
 	}
 	
 	/**
 	 * Removes a conference from a list.
+	 * 
+	 * if the given conference is not being managed,
+	 * the method will return false without effecting anything
+	 * 
 	 * @param c conference object
+     * @throws NullPointerException if the given conference is null.
 	 * @return if the conference was successfully removed
 	 */
-	boolean removeConference(Conference c) {
-		return false;
+	boolean removeConference(Conference c) throws NullPointerException {
+	    if (c == null) {
+            throw new NullPointerException("Cannot remove null conference!");
+        }
+	    
+		return conferences.remove(c);
 	}
 	
 	/**
@@ -33,7 +64,7 @@ public class ConferenceManager implements Serializable {
 	 * @return list of conferences
 	 */
 	List<Conference> getConferences() {
-		return null;
+		return new ArrayList<Conference>(conferences);
 	}
 	
 }
