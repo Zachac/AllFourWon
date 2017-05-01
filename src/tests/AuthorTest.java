62422cb3e1afb2a97e128c136ecc234f6591dde8
Zachary Chandler <Zachary.A.Chandler96@gmail.com>
2017-04-30 17:22:54 -0700

    removed login methods from author test

--- src/tests/AuthorTest.java
+++ src/tests/AuthorTest.java
@@ -16,23 +16,7 @@ public class AuthorTest {
 		String authorName = "Brian";
 		Author brian = new Author(authorName);
 		
-		assertTrue(brian.getName().equals(authorName));
-		assertFalse(brian.login(authorName));
-	}
-	
-	/**
-	 * Test login information.
-	 */
-	@Test
-	public void testLogin() {
-		String loginInfo = "b41naosp";
-		Author brian = new Author("Brian");
-		
-		assertFalse(brian.login(loginInfo));
-		
-		brian.setLogin(loginInfo);
-		
-		assertTrue(brian.login(loginInfo));
+		assertTrue(brian.getUser().equals(authorName));
 	}
 
 }
