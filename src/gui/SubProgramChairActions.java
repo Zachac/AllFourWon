61702b3216ffc234e9775244215593c393ba3f60
Dmitriy Bliznyuk <dimabliz@gmail.com>
2017-05-06 15:48:24 -0700

    Working on the assignReviewer method

--- src/gui/SubProgramChairActions.java
+++ src/gui/SubProgramChairActions.java
@@ -1,9 +1,21 @@
 package gui;
 
+import java.io.PrintStream;
+import java.util.Scanner;
+
+import model.Author;
+import model.Conference;
+
 public class SubProgramChairActions {
 	
 	public static void assignReviewer(UserInfo info) {
-		
+		PrintStream out = info.out;
+        Scanner in = info.in;
+        Conference currentConference = info.getCurrentConference();
+        
+        //currentConference.
+        out.println("Enter the associated number of the paper you want to assign a Reviewer to (or 0 to cancel): ");
+        
 	}
 
 	
